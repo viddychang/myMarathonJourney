d27c26ae5de55f51247b5a6612266dfafbdbf652
dchang11194 <dchang11194@gmail.com>
2021-03-29 19:47:20 -0400

    fixed findMarathonById in marathonService

--- src/services/marathon-service.js
+++ src/services/marathon-service.js
@@ -7,14 +7,14 @@ const findMarathonsByName = (n) => {
 // make sure that 's' values are two letter abbreviations
 const findMarathonsByState = (s) => {
     return fetch(
-        `https://runsignup.com/rest/races?format=json&api_key=Me9AhLY9kvuc2GAKyRtFckMGy9nZVzXI&api_secret=oqqWWM6kf3iB9GuxcemQSdYRZu9cRBQk&state=${s}`)
+        `https://hidden-mountain-72737.herokuapp.com/https://runsignup.com/rest/races?format=json&api_key=Me9AhLY9kvuc2GAKyRtFckMGy9nZVzXI&api_secret=oqqWWM6kf3iB9GuxcemQSdYRZu9cRBQk&state=${s}`)
         .then(response => response.json())
         
 }
 
 const findMarathonById = (id) => {
     return fetch(
-        `https://runsignup.com/rest/race/${id}?format=json&api_key=Me9AhLY9kvuc2GAKyRtFckMGy9nZVzXI&api_secret=oqqWWM6kf3iB9GuxcemQSdYRZu9cRBQk`)
+        `https://hidden-mountain-72737.herokuapp.com/https://runsignup.com/rest/race/${id}?format=json&api_key=Me9AhLY9kvuc2GAKyRtFckMGy9nZVzXI&api_secret=oqqWWM6kf3iB9GuxcemQSdYRZu9cRBQk`)
         .then(response => response.json())
 }
 
