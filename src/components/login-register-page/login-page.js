abb711fdfae4bbc468127a76c1cbfe6cc10fae9a
dchang11194 <dchang11194@gmail.com>
2021-04-26 21:22:59 -0400

    small changes

--- src/components/login-register-page/login-page.js
+++ src/components/login-register-page/login-page.js
@@ -24,7 +24,7 @@ const Login = () => {
                 }
             })
             .then(userService.getCurrentUser().then((user)=> console.log(user)))
-            .then(
+            .catch(
                 toast.error("Sign in failed. Please try again later.", {
                     position: "top-center",
                     autoClose: 5000,
